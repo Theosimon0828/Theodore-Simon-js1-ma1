1bf68b458f523e4dab035693a9eace9b972d5ce7
Theosimon0828 <63924343+Theosimon0828@users.noreply.github.com>
2020-08-16 23:02:30 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,54 @@
+const cats = [
+    {
+        name: "Blob",
+        age: 10
+    },
+    {
+        name: "Harold",
+    },
+    {
+        name: "Blurt",
+        age: 21
+    }
+];
+
+//Question 1
+const cat = {
+    complain: function() {
+        console.log("Mweow!");
+    }
+
+};
+cat.complain();
+
+//Question 2
+const heading = document.querySelector("h3");
+heading.innerHTML = "Updated heading";
+
+//Question 3
+heading.style.fontSize = "2em";
+
+//Question 4
+heading.classList.add("subheading");
+console.dir(heading.classList);
+
+heading.className = "heading subheading";
+console.log(heading.className);
+
+//Question 5
+const paragraphs = document.querySelectorAll("p");
+    //console.log(paragraphs);
+for (let i=0; i<paragraphs.length; i++){
+    //console.log(paragraphs[i]);
+    paragraphs[i].style.color = "red";
+}
+
+//Question 6
+const resultsContainer = document.querySelector(".results");
+
+resultsContainer.innerHTML = "<p>New paragraph</p>";
+    //console.dir(resultsContainer);
+resultsContainer.style.backgroundColor = "yellow";
+
+//Question 7
+
